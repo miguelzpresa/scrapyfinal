61136e89c77a4da0737215e0426704c847978ff3
miguelzpresa <mikezpresa@gmail.com>
2023-06-28 09:14:16 -0600

    antes de la  tragedia

--- p44/p44/items.py
+++ p44/p44/items.py
@@ -2,16 +2,24 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class P44Item(scrapy.Item):
-    # define the fields for your item here like:
-    name_ = scrapy.Field()
+    area = scrapy.Field()
+    pobla = scrapy.Field()
+    gdp_percapita = scrapy.Field()
+    unemploymentrate = scrapy.Field()
+    Taxes_a_other_v = scrapy.Field()
+    debt_ext = scrapy.Field()
+    exchange = scrapy.Field()
+    internet_Total = scrapy.Field()
+    internet_percent__popu = scrapy.Field()
+    airports = scrapy.Field()
+    roadways_total = scrapy.Field()
+    mili_expenditures = scrapy.Field()
     urls_ = scrapy.Field()
-#def main():
- #   pass
+
         
 #if __name__==__main__():
  #   main()
--- p44/p44/spiders/araña3.py
+++ p44/p44/spiders/araña3.py
@@ -1,7 +1,7 @@
 import scrapy
 import json
 import time
-import csv
+from scrapy.exporters import CsvItemExporter
 from p44.items import P44Item
 from urllib.parse import urljoin
 import random 
@@ -13,28 +13,31 @@ class Spiderr(scrapy.Spider):
     download_delay = 1.0 + random.random() * 3.0
 
     def __init__(self):
-        super(Spiderr,self).__init__()
-        self.lista_urls= list()
+        super(Spiderr, self).__init__()
+        self.lista_urls = list()
         self.names = list()
-        self.csvfile = open("../data.csv","a",newline="")
-        self.writer = csv.DictWriter(self.csvfile, fieldnames=[
-        "area","pobla","gdp_percapita","unemploymentrate","Taxes_a_other_v","debt_ext","exchange","internet_Total","internet_percent__popu","airports","roadways_total","mili_expenditures",])
-        if self.csvfile.tell() == 0:
-            self.writer.writeheader()
-
+        self.csvfile = open("../data.csv", "ab")
+        self.exportador = CsvItemExporter(self.csvfile)
+        self.exportador.fields_to_export = [
+            "area", "pobla", "gdp_percapita", "unemploymentrate", "Taxes_a_other_v",
+            "debt_ext", "exchange", "internet_Total", "internet_percent__popu",
+            "airports", "roadways_total", "mili_expenditures"
+        ]
+        self.exportador.start_exporting()
+       
 
     def closed(self, reason):
+        self.exportador.finish_exporting()
         self.csvfile.close()
-    def parse(self,response):
-        #names = list() 
-        
+
+    def parse(self, response):
         first_slice_url = "https://www.cia.gov/the-world-factbook/"
-        uri_values =list()
+        uri_values = list()
         data = json.loads(response.body)
         for country in data['data']['allWordpressWpCountry']['nodes']:
             uri_values.append(country['uri'])
-            new_url= urljoin(first_slice_url, country["uri"])
-            self.lista_urls.append(new_url )
+            new_url = urljoin(first_slice_url, country["uri"])
+            self.lista_urls.append(new_url)
             print(new_url)
             self.names.append(country["title"])
 
@@ -42,27 +45,37 @@ class Spiderr(scrapy.Spider):
         item['urls_'] = self.lista_urls
         
         for country in self.lista_urls[:3]:
-            yield scrapy.Request(country ,callback=self.segunda_fase)
+            yield scrapy.Request(country, callback=self.segunda_fase)
             time.sleep(random.uniform(1, 5))
     
 
-    def segunda_fase(self,response):
+    def segunda_fase(self, response):
         area = response.xpath('''//*[@id="geography"]/div[4]/p/text()[1]''')  
-        pobla =  response.xpath('''//*[@id="people-and-society"]/div[1]/p''')
+        pobla = response.xpath('''//*[@id="people-and-society"]/div[1]/p''')
         gdp_percapita = response.xpath('''/html/body/div/div[1]/div[2]/main/div[2]/section/div/div/div[2]/div[6]/div[4]/p/text()[1]''')
         unemploymentrate = response.xpath('''//html/body/div/div[1]/div[2]/main/div[2]/section/div/div/div[2]/div[6]/div[14]/p/text()[1]''')
-        Taxes_a_other_v= response.xpath('''//*[@id="economy"]/div[22]/p''')
+        Taxes_a_other_v = response.xpath('''//*[@id="economy"]/div[22]/p''')
         debt_ext = response.xpath('''//*[@id="economy"]/div[32]/p''')
         exchange = response.xpath('''//*[@id="economy"]/div[33]/p/text()[2]''')
         internet_Total = response.xpath('''//*[@id="communications"]/div[6]/p/text()[1]''')
-        internet_percent__popu= response.xpath('''//*[@id="communications"]/div[6]/p/text()[2]''')
+        internet_percent__popu = response.xpath('''//*[@id="communications"]/div[6]/p/text()[2]''')
         airports = response.xpath('''//*[@id="transportation"]/div[3]/p''')
         roadways_total = response.xpath('''//*[@id="transportation"]/div[8]/p/text()[1]''')
         mili_expenditures = response.xpath('''//*[@id="military-and-security"]/div[2]/p/text()[1]''')
-        data = { "area":area,"pobla":pobla,"gdp_percapita":gdp_percapita,"unemploymentrate":unemploymentrate,"Taxes_a_other_v":Taxes_a_other_v,"debt_ext":debt_ext,"exchange":exchange,"internet_Total":internet_Total,"internet_percent__popu":internet_percent__popu,"airports":airports,"roadways_total":roadways_total,"mili_expenditures":mili_expenditures}
 
-        self.writer.writerow(data)
+        item = P44Item()
+        item['area'] = area
+        item['pobla'] = pobla
+        item['gdp_percapita'] = gdp_percapita
+        item['unemploymentrate'] = unemploymentrate
+        item['Taxes_a_other_v'] = Taxes_a_other_v
+        item['debt_ext'] = debt_ext
+        item['exchange'] = exchange
+        item['internet_Total'] = internet_Total
+        item['internet_percent__popu'] = internet_percent__popu
+        item['airports'] = airports
+        item['roadways_total'] = roadways_total
+        item['mili_expenditures'] = mili_expenditures
 
-
-        
+        yield item
 
